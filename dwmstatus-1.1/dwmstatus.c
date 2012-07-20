fd098983c67769dfd84eb574b9820a9056959c9a
Kyle Terry <kyle@kyleterry.com>
2012-07-20 01:13:20 -0700

    Added volume percentage to dwmstatus program

--- dwmstatus-1.1/dwmstatus.c
+++ dwmstatus-1.1/dwmstatus.c
@@ -1,4 +1,4 @@
-#define _BSD_SOURCE
+#define _GNU_SOURCE
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,10 +12,71 @@
 
 #include <X11/Xlib.h>
 
+#include <fcntl.h>
+#include <alsa/asoundlib.h>
+
 char *tzutc = "UTC";
 char *tzamericapacific = "America/Los_Angeles";
 
 static Display *dpy;
+
+
+int
+audio_volume(long* outvol)
+{
+    int ret = 0;
+    snd_mixer_t* handle;
+    snd_mixer_elem_t* elem;
+    snd_mixer_selem_id_t* sid;
+
+    static const char* mix_name = "Master";
+    static const char* card = "default";
+    static int mix_index = 0;
+
+    snd_mixer_selem_id_alloca(&sid);
+
+    //sets simple-mixer index and name
+    snd_mixer_selem_id_set_index(sid, mix_index);
+    snd_mixer_selem_id_set_name(sid, mix_name);
+
+        if ((snd_mixer_open(&handle, 0)) < 0)
+        return -1;
+    if ((snd_mixer_attach(handle, card)) < 0) {
+        snd_mixer_close(handle);
+        return -2;
+    }
+    if ((snd_mixer_selem_register(handle, NULL, NULL)) < 0) {
+        snd_mixer_close(handle);
+        return -3;
+    }
+    ret = snd_mixer_load(handle);
+    if (ret < 0) {
+        snd_mixer_close(handle);
+        return -4;
+    }
+    elem = snd_mixer_find_selem(handle, sid);
+    if (!elem) {
+        snd_mixer_close(handle);
+        return -5;
+    }
+
+    long minv, maxv;
+
+    snd_mixer_selem_get_playback_volume_range (elem, &minv, &maxv);
+
+    if(snd_mixer_selem_get_playback_volume(elem, 0, outvol) < 0) {
+        snd_mixer_close(handle);
+        return -6;
+    }
+
+    /* make the value bound to 100 */
+    *outvol -= minv;
+    maxv -= minv;
+    minv = 0;
+    *outvol = 100 * (*outvol) / maxv; // make the value bound from 0 to 100
+    snd_mixer_close(handle);
+    return 0;
+}
 
 char *
 smprintf(char *fmt, ...)
@@ -161,27 +222,26 @@ main(void)
 	char *status;
 	char *avgs;
 	char *bat;
-	char *tmar;
 	char *tmutc;
 	char *tmbln;
+    long vol = -1;
 
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "dwmstatus: cannot open display.\n");
 		return 1;
 	}
 
-	for (;;sleep(5)) {
+	for (;;sleep(2)) {
 		avgs = loadavg();
 		bat = getbattery("/proc/acpi/battery/BAT0");
 		tmutc = mktimes("%H:%M", tzutc);
-		tmbln = mktimes("KW %W %a %d %b %H:%M %Z %Y", tzamericapacific);
-
-		status = smprintf("[L: %s | Bat: %s%% | UTC: %s | %s]",
-				avgs, bat, tmutc, tmbln);
+		tmbln = mktimes("%W %a %d %b %H:%M %Z %Y", tzamericapacific);
+        audio_volume(&vol);
+		status = smprintf("[L: %s | Bat: %s%% | Vol: %i%% | UTC: %s | %s]",
+				avgs, bat, vol, tmutc, tmbln);
 		setstatus(status);
 		free(avgs);
 		free(bat);
-		free(tmar);
 		free(tmutc);
 		free(tmbln);
 		free(status);
